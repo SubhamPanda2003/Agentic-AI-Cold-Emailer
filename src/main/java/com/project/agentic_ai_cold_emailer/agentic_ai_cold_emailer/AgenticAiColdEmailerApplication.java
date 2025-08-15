15e37a2764eaf88df72442a7eb9cf09d9673207b
SubhamPanda2003 <sajalkumarpanda@gmail.com>
2025-08-15 12:05:21 +0530

    initial setup

---
+++ src/main/java/com/project/agentic_ai_cold_emailer/agentic_ai_cold_emailer/AgenticAiColdEmailerApplication.java
@@ -0,0 +1,13 @@
+package com.project.agentic_ai_cold_emailer.agentic_ai_cold_emailer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgenticAiColdEmailerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgenticAiColdEmailerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/agentic_ai_cold_emailer/agentic_ai_cold_emailer/AgenticAiColdEmailerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.agentic_ai_cold_emailer.agentic_ai_cold_emailer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgenticAiColdEmailerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
